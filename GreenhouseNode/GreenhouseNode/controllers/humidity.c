ad1f452c7731ffe8b3ad202e854aec6ba515a507
Michel Sommer <skruen@gmail.com>
2021-12-10 13:21:09 +0100

    review, impl hih8120 driver

--- GreenhouseNode/GreenhouseNode/controllers/humidity.c
+++ GreenhouseNode/GreenhouseNode/controllers/humidity.c
@@ -4,26 +4,55 @@
  * Created: 12/6/2021 1:56:52 PM
  *  Author: tolde
  */ 
-#include <stdio.h>
-#include "humidity.h"
 
-typedef struct humidity{
-	int8_t latestHumidity;
-	}humidity;
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <hih8120.h>
+
+#include "humidity.h"
 
 humidity_t humidity_create(void)
 {
+	
+	hih8120_driverReturnCode_t returnCode = hih8120_initialise();
+		
 	humidity_t newHumidity = calloc(1, sizeof(humidity));
-	newHumidity ->latestHumidity = 0;
-	return newHumidity;
+	newHumidity->latestHumidity = 0;
+		
+	if (returnCode == HIH8120_OK)
+	{
+		// success
+		return newHumidity;
+	}
+	else
+	{
+		printf("\nHIH8120_TEMP_SENSOR_ERROR: %t\n", returnCode);
+		free(newHumidity);
+		return NULL;		// catch this
+	}
+		
 }
 
-void humidity_measure(humidity_t sensor_h)
+humidity_t humidity_measure(humidity_t sensor)
 {
-	sensor_h ->latestHumidity;
-}
+	
+	hih8120_driverReturnCode_t returnCode;
+	returnCode = hih8120_wakeup()
+		
+	if ( returnCode == HIH8120_OK )
+	{
+		// Something went wrong
+		// Investigate the return code further
+			
+		int16_t humidity = (int16_t) hih8120_getHumidityPercent_x10();
+		sensor->latestHumidity = humidity;
+	}
+	else
+	{
+		printf("\nHIH8120_TEMP_SENSOR_READING_ERROR: %t\n", returnCode);
+	}
 
-uint8_t humidity_getHumidity(humidity_t h_sensor)
-{
-  return h_sensor->latestHumidity;
+	return sensor;
+		
 }

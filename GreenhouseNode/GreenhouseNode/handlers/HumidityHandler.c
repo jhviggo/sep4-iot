6ec1894da4f71c7b5dab232035b473031cbafd9e
Florina Toldea <toldea.florina1712@yahoo.com>
2021-12-13 17:13:40 +0100

    no errors, but feel free to checkout

---
+++ GreenhouseNode/GreenhouseNode/handlers/HumidityHandler.c
@@ -0,0 +1,121 @@
+/*
+ * HumidityHandler.c
+ *
+ * Created: 12/9/2021 1:25:54 PM
+ *  Author: tolde
+ */ 
+#include <ATMEGA_FreeRTOS.h>
+#include <semphr.h>
+#include <rc_servo.h>
+#include <stdio.h>
+#include <hih8120.h>
+#include <task.h>
+#include "event_groups.h"
+#include "HumidityHandler.h"
+
+EventGroupHandle_t task_startGroup;
+EventBits_t measureBit;
+
+
+void start_humidity_task(void* self);
+
+void humidity_handler_initialise(UBaseType_t humidityHandler_priority, humidityHandler_t self)
+{
+	xEventGroupSetBits(task_startGroup, measureBit);
+	
+	xTaskCreate(
+	start_humidity_task
+	, "HumidityTask"
+	,configMINIMAL_STACK_SIZE + 100
+	, (void*)self
+	,humidityHandler_priority
+	,NULL
+	);
+}
+
+void start_humidity_task(void* self)
+{
+	TickType_t xLastWorkingTime;
+	const TickType_t xFrequency = pdMS_TO_TICKS(15000UL);
+	xLastWorkingTime = xTaskGetTickCount();
+	const TickType_t xFrequency1 = pdMS_TO_TICKS(100UL);
+	
+	for(;;)
+	{
+		xTaskDelayUntil(&xLastWorkingTime, xFrequency);
+		
+		if(HIH8120_OK != hih8120_wakeup())
+		{
+			printf("Task FAILED");
+		}
+		xTaskDelayUntil(&xLastWorkingTime, xFrequency1);
+		humidity_handler_task((humidityHandler_t)self);
+	}
+}
+
+humidityHandler_t humidityHandler_create(UBaseType_t hum_task_priority, EventGroupHandle_t eventBits, EventBits_t bits)
+{
+	humidityHandler_t newReader = calloc(1, sizeof(humidityHandler));
+	
+	if(newReader == NULL)
+	{
+		return NULL;
+	}
+	
+	newReader->humidity = 0;
+	
+	measureBit = bits;
+	
+	task_startGroup = eventBits;
+	
+	if ( humidity_create() == NULL)
+	{
+		printf("\nHIH8120_HUM_SENSOR_ERROR: Sensor not initialized\n");
+	}
+	else
+	{
+		humidityHandler_t humiditySensor = humidity_create();
+	}
+	
+	if(HIH8120_OK == hih8120_initialise())
+	{
+		printf("Humidity sensor is INITIALIZED");
+	}
+	
+	humidity_handler_initialise(hum_task_priority, newReader);
+	
+	return newReader;
+}
+
+humidityHandler_t humidityHandler_destroy(humidityHandler_t self)
+{
+	 humidity_destroy(self);
+}
+
+uint16_t humidityHandler_getHumidity(humidityHandler_t h_sensor)
+{
+	humidity_getHumidity(h_sensor);
+}
+
+void humidity_handler_task(humidityHandler_t self)
+{
+	EventBits_t uxBits = xEventGroupWaitBits(
+	task_startGroup,
+	measureBit,
+	pdFALSE,
+	pdTRUE,
+	portMAX_DELAY);
+	
+	if((uxBits & (measureBit) == (measureBit)))
+	{
+		if(HIH8120_OK != hih8120_measure())
+		{
+			printf("Humidity task FAILED");
+		}
+		else
+		{
+			self->humidity = hih8120_getHumidity();
+			printf("Humidity: %f\n", self ->humidity);
+		}
+	}
+}
---
+++ GreenhouseNode/GreenhouseNode/handlers/HumidityHandler.h
@@ -0,0 +1,28 @@
+/*
+ * HumidityHandler.h
+ *
+ * Created: 12/9/2021 1:26:07 PM
+ *  Author: tolde
+ */ 
+#pragma once
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include "event_groups.h"
+
+typedef struct humidityHandler
+{
+	uint16_t humidity;
+	uint16_t humidityToDestroy;
+}humidityHandler;
+
+
+typedef struct humidityHandler* humidityHandler_t;
+
+void start_humidity_task(void* self);
+uint16_t humidityHandler_getHumidity(humidityHandler_t self);
+humidityHandler_t humidityHandler_create(UBaseType_t hum_task_priority, EventGroupHandle_t eventBits, EventBits_t bits);
+humidityHandler_t humidityHandler_destroy(humidityHandler_t self);
+
